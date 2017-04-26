cf5695c6b1dc9dd8852defdf3a4cb545760bec94
Harry-Kwon <sxk1066@case.edu>
2017-04-26 16:17:52 -0400

    added regex matching to user nicknames and create list of all users with unformatted nicknames in guild

--- botscript.py
+++ botscript.py
@@ -1,15 +1,33 @@
-import requests, json
+import requests, json, re
 
+def name_is_formatted(name, pattern):
+	print(name)
+	if pattern.match(name):
+		print(True)
+		return(True)
+	else:
+		print(False)
+		return(False)
+
+def get_unformatted_users(guild_id, bot_token, name_pattern):
+	#set request params
+	url = 'https://discordapp.com/api/guilds/' + guild_id + '/members'
+	auth_string = 'Bot %s' % bot_token
+	headers = {'Authorization' : auth_string}
+	#make request to get list of all users
+	r = requests.get(url, headers = headers)
+	member_list = json.loads(r.content.decode('utf-8'))
+	#add unformatted users to list
+	unformatted_users = []
+	for x in member_list:
+		nickname = x['nick']
+		if not name_is_formatted(nickname, name_pattern):
+			unformatted_users.append(x)
+
+	return(unformatted_users)
+
+pattern = re.compile("^.*\(.*\)$")
 guild_id = "260589757396942849"
-url = 'https://discordapp.com/api/guilds/' + guild_id + '/members'
-
 bot_token = 'MzA2ODQyNzE5MDY4NzQ5ODMy.C-Jwwg.gCOVKnFnOe9A85I2cUY9yPHOhDc'
-auth_string = 'Bot %s' % bot_token
 
-print(auth_string)
-
-headers = {'Authorization' : auth_string}
-
-r = requests.get(url, headers = headers)
-member_list = json.loads(r.content.decode('utf-8'))
-print(member_list[0])
+print( get_unformatted_users(guild_id, bot_token, pattern))
